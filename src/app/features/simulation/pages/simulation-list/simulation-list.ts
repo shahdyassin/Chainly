9fd1b56d2964be20dcae33967aeff8b6f224c1c3
Shahd Yassin <shahdyassin63@gmail.com>
2026-04-21 23:34:47 +0200

    feat: Add simulation list page and routing

---
+++ src/app/features/simulation/pages/simulation-list/simulation-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simulation-list',
+  imports: [],
+  templateUrl: './simulation-list.html',
+  styleUrl: './simulation-list.scss',
+})
+export class SimulationList {
+
+}
